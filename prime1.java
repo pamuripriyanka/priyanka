0aaa0fa8d6be0fb8123789ff4be15af5d69bdc50
pamuripriyanka <41770146+pamuripriyanka@users.noreply.github.com>
2018-07-31 02:28:56 -0700

    Create prime1.java

---
+++ prime1.java
@@ -0,0 +1,37 @@
+/* package whatever; // don't place package name! */
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class prime1
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int i =0,num=0;
+		//Empty String
+		String  primeNumbers = "";
+		for (i = 1; i <= 10; i++)        
+		{ 		  	  
+			int counter=0; 	  
+			for(num =i; num>=1; num--)
+			{
+            			if(i%num==0)
+				{
+ 					counter = counter + 1;
+				}
+			 }
+			if (counter ==2)
+			{
+				//Appended the Prime number to the String
+				primeNumbers = primeNumbers + i + " ";
+			}
+		}
+				
+		
+		System.out.println("Prime numbers from 1 to 10 are :");
+		System.out.println(primeNumbers);
+	}
+}
+
+	
