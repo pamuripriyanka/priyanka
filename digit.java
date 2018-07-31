f36795c9bc812ec28a1575b185b50c6447ef7625
pamuripriyanka <41770146+pamuripriyanka@users.noreply.github.com>
2018-07-30 22:20:23 -0700

    Create digit.java

---
+++ digit.java
@@ -0,0 +1,24 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int Number, Count=0;
+		 Scanner sc=new Scanner(System.in);
+	   	sc = new Scanner(System.in);		
+		 Number = sc.nextInt();
+		 while(Number > 0) 
+		 {
+			Number = Number / 10;
+			Count = Count + 1; 
+		 }
+		 System.out.println(Count);
+	}
+
+}
