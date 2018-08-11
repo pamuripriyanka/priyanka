e84c68e4f6695cc28adfd3b10084823dd04363f3
pamuripriyanka <41770146+pamuripriyanka@users.noreply.github.com>
2018-08-11 00:24:22 -0700

    Update arithmetic.java

--- arithmetic.java
+++ arithmetic.java
@@ -10,25 +10,10 @@ class arithmetic
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		Scanner input = new Scanner(System.in);
-		double firstTerm = 0;
-    		double numberOfTerms = 0;
-    		double commonDifference = 0;
-    		double  sum= 0;
-    		double term = 0;
-			System.out.print("Enter the value of a (First Term) : "); 
-    	firstTerm = input.nextDouble();
-			System.out.print("Enter the value of d (Common Difference) : "); 
-    	commonDifference = input.nextDouble();
-			System.out.print("Enter the value of n (Number of terms) : "); 
-    	numberOfTerms = input.nextDouble();
-		sum = numberOfTerms * (2 * firstTerm + (numberOfTerms - 1) * commonDifference)/2;
-		System.out.println("");
-		for(int i = 0; i < numberOfTerms; i++)
-		{
-    			term = firstTerm + i * commonDifference;
-    			System.out.print(term+" + ");
-    		}
-		System.out.println("...");
-    		System.out.println("The Sum of n terms of series : " + sum);
+		int sum=0;
+		int N=3,A=1,D=1;
+		sum=N*(2*A+(N-1)*D)/2;
+    		System.out.println(" "+sum);
 	}
+}
 }
