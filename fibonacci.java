4698cef15fb2768f3cade183d71314d8c7509de9
pamuripriyanka <41770146+pamuripriyanka@users.noreply.github.com>
2018-08-13 00:03:32 -0700

    Update fibonacci.java

--- fibonacci.java
+++ fibonacci.java
@@ -1,10 +1,6 @@
-* package whatever; // don't place package name! */
-
 import java.util.*;
 import java.lang.*;
 import java.io.*;
-
-/* Name of the class has to be "Main" only if the class is public. */
 class fibonacci
 {
 	public static void main (String[] args) throws java.lang.Exception
