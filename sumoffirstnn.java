036f3b5e054c63a5b699f409c40f16ce6902ca9d
pamuripriyanka <41770146+pamuripriyanka@users.noreply.github.com>
2018-08-17 08:13:19 -0700

    Update sumoffirstnn.java

--- sumoffirstnn.java
+++ sumoffirstnn.java
@@ -7,17 +7,18 @@ import java.io.*;
 /* Name of the class has to be "Main" only if the class is public. */
 class Ideone
 {
+	static int findSum(int n)
+	{
+		int sum = 0;
+		for (int x = 1; x <= n; x++) 
+            	sum=sum+x;
+        		return sum;
+	}
 	public static void main (String[] args) throws java.lang.Exception
 	{
-		int x,i=1;
-		int sum=0;
+		int n;
 		Scanner sc=new Scanner(System.in);
-		x=sc.nextInt();
-		while(i<=x)
-		{
-            		sum=sum+i;
-            		i++;
-		}
-		System.out.println(" "+sum);
+		n=sc.nextInt();
+		System.out.println(findSum(n));
 	}
 }
