69f9ef3bd2b2576f9df92853d723b2bbeb9344c4
pamuripriyanka <41770146+pamuripriyanka@users.noreply.github.com>
2018-08-18 08:20:39 -0700

    Create ascii.java

---
+++ ascii.java
@@ -0,0 +1,18 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		char ch=sc.next().charAt(0);
+		int ascii=ch;
+		int castAscii=(int) ch;
+		System.out.println(ascii);
+	}
+}
