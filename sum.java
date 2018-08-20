30de1ea97bd2ea38ab2ca2274dfe6ae5ad16ce6c
pamuripriyanka <41770146+pamuripriyanka@users.noreply.github.com>
2018-08-19 23:24:55 -0700

    Update sum.java

--- sum.java
+++ sum.java
@@ -5,15 +5,16 @@ import java.lang.*;
 import java.io.*;
 
 /* Name of the class has to be "Main" only if the class is public. */
-class Ideone
+class sum
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
+		* package whatever; // don't place package name! */
+		int x,y,z;
 		Scanner sc=new Scanner(System.in);
-		int n1=sc.nextInt();
-		int n2=sc.nextInt();
-		int sum;
-		sum=n1+n2;
-		System.out.println(sum);
+		x=sc.nextInt();
+		y=sc.nextInt();
+		z=x+y;
+		System.out.println(" "+z);
 	}
 }
