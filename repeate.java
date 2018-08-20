83af4c6525d4c437950958f6f6fca6109b8c10ed
pamuripriyanka <41770146+pamuripriyanka@users.noreply.github.com>
2018-08-19 23:54:28 -0700

    Update repeate.java

--- repeate.java
+++ repeate.java
@@ -5,16 +5,16 @@ import java.lang.*;
 import java.io.*;
 
 /* Name of the class has to be "Main" only if the class is public. */
-class Ideone
+class string
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
-		Scanner sc=new Scanner(System.in);
-		String ch=sc.nextLine();
-		int a=sc.nextInt();
+		Scanner scan=new Scanner(System.in);
+		String s=scan.nextLine();
+		int a=scan.nextInt();
 		for(int i=0;i<a;i++)
 		{
-			System.out.println(ch);
+			System.out.println(s);
 		}
 	}
 }
