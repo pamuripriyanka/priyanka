edbe23bf9ba4b9925d75fed70233143972458e68
pamuripriyanka <41770146+pamuripriyanka@users.noreply.github.com>
2018-08-22 23:22:19 -0700

    Create withoutspace.java

---
+++ withoutspace.java
@@ -0,0 +1,18 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int n1,n2;
+		Scanner sc=new Scanner(System.in);
+		n1=sc.nextInt();
+		n2=sc.nextInt();
+		System.out.println(n1+""+n2);
+	}
+}
