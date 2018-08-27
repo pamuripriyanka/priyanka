2225caa6f80939b5d6cf85d1534b9998ac226a9a
pamuripriyanka <41770146+pamuripriyanka@users.noreply.github.com>
2018-08-27 09:58:14 +0530

    Create specialchar.java

---
+++ specialchar.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		String str=sc.nextLine();
+		int count=0;
+		for(int i=0;i<str.length()-1;i++)
+		{
+			if(!Character.isDigit(str.charAt(i))&&!Character.isAlphabetic(str.charAt(i))&&str.charAt(i)!=' ')
+			{
+				count++;
+			}	
+		}
+		System.out.println(count);
+	}
+}
+	
