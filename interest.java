f1f2e4e76ca8e8ac7e315496bf7a542ace006e43
pamuripriyanka <41770146+pamuripriyanka@users.noreply.github.com>
2018-08-30 10:55:25 +0530

    Create interest.java

---
+++ interest.java
@@ -0,0 +1,20 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int P,T,R;
+		Scanner sc=new Scanner(System.in);
+		P=sc.nextInt();
+		T=sc.nextInt();
+		R=sc.nextInt();
+		int SI=(P*T*R)/100;
+		System.out.println(SI);
+	}
+}
